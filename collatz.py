593ab92142f4db391186e0bffce39c80a64a5047
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2017-11-16 02:13:43 +0600

    Positive and Negative Number
    This works for both negative and positive number.

--- collatz.py
+++ collatz.py
@@ -1,23 +1,29 @@
-def collatz(number):
-    #Is this a even number
-    if number % 2 == 0:
-        b = number // 2
-        return b
-    #This one is for odd number
+#This is for positive number
+def collatzPositive(positiveNumber):
+    if positiveNumber % 2 == 0:
+        print(positiveNumber // 2)
+        return positiveNumber // 2
     else:
-        b = 3 * number + 1
-        return b
+        print(positiveNumber * 3 + 1)
+        return positiveNumber * 3 + 1
+
+#This is for negative number    
+def collatzNegative(negativeNumber):
+    if negativeNumber % 2 == 0:
+        print(negativeNumber // 2)
+        return negativeNumber // 2
+    else:
+        print(negativeNumber * 3 - 1)
+        return negativeNumber * 3 - 1
     
-#Will take a number from you
-n = input('Please, give me a number: ')
-
-#Do a list of sequence
-sequence = []
-
-#This loop will contune untill it gets 1
-while n != 1:
-    n = collatz(int(n))
-    sequence.append(n)
-
-#This will print  
-print (sequence)
+#Starts
+try:
+    givenNumber = int(input('Enter number: '))
+    if givenNumber > 0:
+        while givenNumber != 1:
+            givenNumber = collatzPositive(givenNumber)
+    else:
+        while givenNumber != -1:
+            givenNumber = collatzNegative(givenNumber)
+except ValueError:
+    print('Type an interger')
